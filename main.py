3218506817a35b9a9467fe5b43bff3fa1ae1ee3f
aresthebellator <andreacreati00@gmail.com>
2025-12-04 08:31:46 +0100

    cambio ia

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-from openai import OpenAI
-from dotenv import load_dotenv
-load_dotenv()
+import base64
+import requests
 
-client = OpenAI()
+def vision_ollama(image_path):
+    with open(image_path, "rb") as f:
+        img = base64.b64encode(f.read()).decode()
 
-input_messages = [
-    {
-        "role": "user",
-        "content": "Ciao come stai"
-    }
-]
+    r = requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": "moondream2",
+            "prompt": "Descrivi l'immagine:",
+            "images": [img]
+        }
+    )
+    return r.json()["response"]
 
-response = client.responses.create(
-    model="gpt-4o-mini",
-    input = input_messages
-)
-
-
-print(response.output_text)
+print(vision_ollama("foto.jpg"))
